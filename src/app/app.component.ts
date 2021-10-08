a4c8d98638a52650f51fb761345e7f8f0b4cb02e
Roberto <mario24roberto@gmail.com>
2021-10-08 11:36:19 -0600

    Subiendo el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Piano';
+  Aplicarsonido(numero:number):void{
+    const audio =new Audio();
+    audio.src='../assets/Sonidos/note' +numero + '.wav';
+    audio.load();
+    audio.play();
+  }
+}
